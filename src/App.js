ee7cc4a0e3872be0491ecbc4510e54c71d63ade7
Sriram Trn <sriram@Srirams-MacBook-Pro-2.local>
2023-05-16 22:49:03 +0530

    Router setup + tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import {router} from './Router';
+
+
+export default function App() {
+  return (
+    <RouterProvider router={router}>
+    <h1 className="text-3xl font-bold underline bg-teal-200">
+      Hello world!
+    </h1>
+    </RouterProvider>
+  )
+}
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,3 @@
+export function Portfolio() {
+    return <div className="bg-blue-300"> Portfolio</div>
+}
